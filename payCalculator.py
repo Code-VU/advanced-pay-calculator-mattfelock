e979ff546d23db22c118b1b4c47ec7192c06226c
Matt Felock <matt.felock@vuhl-t6jk20n.vuhl.root.mrc.local>
2022-01-28 11:17:15 -0600

    entered code to calculate pay with overtime

--- payCalculator.py
+++ payCalculator.py
@@ -1,7 +1,15 @@
 def calculatePay():
     
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+    hrs = input("Enter Hours: ")
+    rate = input("Enter Rate: ")
+    h = float(hrs)
+    r = float(rate)
+    if h <= 40:
+        pay = h * r
+    else:
+        pay = (h - 40) * (r * 1.5) + (40 * r)
+    print(pay)
     
     # end assignment
 
@@ -9,4 +17,4 @@ def calculatePay():
 ## uncomment calculatePay() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-# calculatePay()
+## calculatePay()
